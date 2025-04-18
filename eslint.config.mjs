da95edd7c96cc890f30d00a9119d628b2a93bedb
Diego Lamarão <diego.lamarao92@gmail.com>
2025-04-18 17:51:51 -0300

    update lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import x2dEslintConfig from '@x2d/eslint-config';
 
 export default defineConfig([
   globalIgnores(['dist', 'node_modules']),
-  { files: ['**/*.{js,mjs,cjs,ts}'], extends: [x2dEslintConfig] },
+  { files: ['**/*.{ts}'], extends: [x2dEslintConfig] },
   { files: ['**/*.{js,mjs,cjs,ts}'], plugins: { js }, extends: ['js/recommended'] },
   { files: ['**/*.{js,mjs,cjs,ts}'], languageOptions: { globals: globals.node } },
   tseslint.configs.recommended,
